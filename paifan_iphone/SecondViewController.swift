fade0c2511d9d11e0389fdf2f62a18f11f60dc8f
daozhihun <xlieo@vip.qq.com>
2016-01-06 20:00:28 +0800

    Empty project.

---
+++ paifan_iphone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  paifan_iphone
+//
+//  Created by 刀之魂 on 16/1/6.
+//  Copyright © 2016年 杭州个范科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
